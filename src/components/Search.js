81c79a42ce49a2af208fd61e1fc5b2f288ba88ae
ZSabakh <zurabsabakhtarishvili@gmail.com>
2020-09-05 21:17:10 -0500

    Update

---
+++ src/components/Search.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+function Search(props) {
+  return (
+    <View style={styles.container}>
+      <AntDesign style={styles.icon} name="search1" size={33} color="black" />
+      <TextInput
+        value={props.search}
+        onChangeText={(newSearch) => props.onSearchChange(newSearch)}
+        style={styles.input}
+        placeholder="Search"
+        onEndEditing={() => onSearchSubmit()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    marginHorizontal: 15,
+    borderRadius: 5,
+    flexDirection: "row",
+  },
+  icon: {
+    alignSelf: "center",
+    marginHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 19,
+  },
+});
+
+export default Search;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Search from "../components/Search";
 
 function SearchScreen() {
+  const [search, setSearch] = useState("");
+  console.log(search);
   return (
     <View>
+      <Search
+        search={search}
+        onSearchChange={(newSearch) => {
+          setSearch(newSearch);
+        }}
+        onSearchSubmit={() => console.log("Great success! Is niiice")}
+      />
       <Text>Henlo</Text>
     </View>
   );
